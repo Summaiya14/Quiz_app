a050285303f9916f70c5c110112f83028ccbc3d0
summaiya14 <summaiyamahmod@gmail.com>
2021-06-02 20:58:31 +0500

    mapping

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,9 +25,19 @@ class MyAppState extends State<MyApp> {
   }
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'what is your fav color',
-      'what is your fav animal',
+    const questions = [
+      {
+        'questionText': 'what is your fav color?',
+        'answers': ['blue', 'red', 'black'],
+      },
+      {
+        'questionText': 'what is your fav dish?',
+        'answers': ['pizza', 'pasta', 'burger'],
+      },
+      {
+        'questionText': 'what is your name?',
+        'answers': ['sums', 'affan', 'faiza'],
+      },
     ];
     return MaterialApp(home: Scaffold(
       appBar: AppBar(
@@ -34,14 +45,10 @@ class MyAppState extends State<MyApp> {
       ),
       body: Column(
         children: [
-          Question(questions[questionIndex]),
-          RaisedButton(child: Text('Answer 1') ,onPressed: answerQuestion),
-          RaisedButton(child: Text('Answer 2') ,onPressed: () => print('ans 2 chosen')),
-          RaisedButton(child: Text('Answer 3') ,onPressed: () {
-            print('Ans 3 chosen');
-          }),
-          
-
+          Question(questions[questionIndex]['questionText']),
+          ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+            return Answer(answerQuestion, answer);
+          }).toList()
         ],
         ),
     ),
